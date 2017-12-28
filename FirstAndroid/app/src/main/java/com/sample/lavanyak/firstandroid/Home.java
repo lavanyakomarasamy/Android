76c0ff158893e5ce561a29926d0e5e7d9bf418a1
lavanya <lavakomarasamy@hotmail.com>
2017-12-28 16:41:59 +0530

    First Android project

---
+++ FirstAndroid/app/src/main/java/com/sample/lavanyak/firstandroid/Home.java
@@ -0,0 +1,34 @@
+package com.sample.lavanyak.firstandroid;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Home extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        TextView b=(TextView)findViewById(R.id.textView);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
